c9a04ab77e383435ab3a60609edaaa09b947f5ee
unknown <naimcse56@gmail.com>
2026-02-22 13:19:32 +0600

    user name

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -29,8 +29,8 @@ import {
 // This is sample data.
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: "Md Naim",
+    email: "naimcse56@gmail.com",
     avatar: "/avatars/shadcn.jpg",
   },
   teams: [
