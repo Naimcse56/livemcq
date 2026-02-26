d9590a635a044d7047278defa31be34afc3c1d3f
unknown <naimcse56@gmail.com>
2026-02-26 14:09:53 +0600

    auth user info show in bottom

--- frontend/src/components/auth/loginForm.tsx
+++ frontend/src/components/auth/loginForm.tsx
@@ -65,10 +65,7 @@ export function LoginForm({
           <form className="p-6 md:p-8" onSubmit={handleSubmit}>
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-                <p className="text-muted-foreground text-balance">
-                  Login to your Acme Inc account
-                </p>
+                <h1 className="text-2xl font-bold">Welcome !!!</h1>
               </div>
 
               <Field>
